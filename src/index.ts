979c1d27c2bfa074b2de8c55d151f5a291ae5c91
严浩 <me@yanhao.ren>
2024-09-02 17:50:17 +0800

    feat: Add deepFreeze utility function for object immutability

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function test() {
-  return 'works!';
-}
+/**
+ * Recursively freeze an object effectively disallowing
+ * changing/adding/editing of its properties.
+ * @param {Record<string, unknown>} obj The object to deep freeze
+ */
+export const deepFreeze = <T extends Record<string, unknown>>(obj: T): T => {
+  const propNames: string[] = Object.getOwnPropertyNames(obj);
+
+  for (const name of propNames) {
+    const value = obj[name] as T;
+
+    if (value && typeof value === 'object') {
+      deepFreeze(value);
+    }
+  }
+
+  return Object.freeze(obj);
+};
