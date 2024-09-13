cc16f8517f8e90f5a19b43d4734d39ffcd3ded3e
严浩 <me@yanhao.ren>
2024-09-13 12:10:15 +0800

    feat: toBase64

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,1 +1,10 @@
+export const toBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener('load', () => resolve(reader.result as string));
+    reader.addEventListener('error', reject);
+  });
+};
+
 export * from './browser-viewport-compat';
